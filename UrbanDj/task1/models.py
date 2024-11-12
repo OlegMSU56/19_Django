6c52c6ea689f685bd27ce0463350f78af7858ed0
OlegMSU56 <sagdeev.ol@yandex.ru>
2024-11-12 23:57:03 +0300

    12/11/24 task2

--- UrbanDj/task1/models.py
+++ UrbanDj/task1/models.py
@@ -21,3 +21,28 @@ class Game(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# >>> from task1.models import Buyer
+# >>> Buyer.objects.create(name='Nagibator404', balance = 100.00, age =17)
+# <Buyer: Nagibator404>
+# >>> Buyer.objects.create(name='Dima', balance = 200.00, age =32)
+# <Buyer: Dima>
+# >>> Buyer.objects.create(name='Petr Prohorov', balance = 300.00, age =20)
+# <Buyer: Petr Prohorov>
+# >>> from task1.models import Game
+# >>> Game.objects.create(title='Mario', cost=52.52, size = 30.00, description = 'Old game', age_limited =False)
+# <Game: Mario>
+# >>> Game.objects.create(title='Mortal Combat', cost=152.20, size = 130.00, description = 'best game', age_limited =True)
+# <Game: Mortal Combat>
+# >>> Game.objects.create(title='Football', cost=100.00, size = 20.00, description = 'popular game', age_limited =True)
+# <Game: Football>
+# >>> buyer1 = Buyer.objects.get(id=1)
+# >>> buyer2 = Buyer.objects.get(id=2)
+# >>> buyer3 = Buyer.objects.get(id=3)
+# >>> game1 = Game.objects.get(id=1)
+# >>> game2 = Game.objects.get(id=2)
+# >>> game3 = Game.objects.get(id=3)
+# >>> game1.buyers.set([buyer1, buyer2, buyer3])
+# >>> game2.buyers.set([buyer2, buyer3])
+# >>> game3.buyers.set([buyer3])
